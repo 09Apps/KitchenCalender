4c04d13b8fd3154939f21c58b88b6fc5e6b51fc7
Swapnil Takalkar <swapniltakalkar@Swapnils-MacBook-Pro.local>
2013-07-15 21:38:41 +0530

    07/15 first build

---
+++ KichenCalender/MilkViewController.h
@@ -0,0 +1,13 @@
+//
+//  MilkViewController.h
+//  KichenCalender
+//
+//  Created by Swapnil Takalkar on 7/15/13.
+//  Copyright (c) 2013 mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MilkViewController : UITableViewController
+
+@end
---
+++ KichenCalender/MilkViewController.m
@@ -0,0 +1,43 @@
+//
+//  MilkViewController.m
+//  KichenCalender
+//
+//  Created by Swapnil Takalkar on 7/15/13.
+//  Copyright (c) 2013 mobi. All rights reserved.
+//
+
+#import "MilkViewController.h"
+
+@interface MilkViewController ()
+
+@end
+
+@implementation MilkViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
